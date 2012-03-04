941f1faf306bb4c38772681b467dd31e0be4ef50
Andre Joseph Cubeta <ajcubeta@gmail.com>
2012-03-05 00:16:18 +0800

    Push rails framework for profile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajcubeta::Application.config.secret_token = '2de42c973baebbf8f9e1367d2f8f4f4b794daa48a8eaa94db53134d9b572d2fae8e3a97d1ed1e9487902750e28840cbbb67434ee87f7de9bda5aa49d0781dc26'
